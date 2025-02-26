1a6e9b38c756f3ab6a76a035f453083d5e845627
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2025-02-26 17:40:27 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    host: '0.0.0.0', // Allows access from any host (useful if you're accessing from an external network)
+    disableHostCheck: true, // Disables host header check (useful for local dev, but not recommended for production)
+          public: 'https://webappvu3deploy.azurewebsites.net', // Use this if you're accessing from a custom domain (if needed)
+  },
+});
