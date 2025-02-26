dccc725a911eb00335967720250ccff4c4bc49ae
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2025-02-26 17:56:56 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 
   devServer: {
     host: '0.0.0.0', // Allows access from any host (useful if you're accessing from an external network)
-    disableHostCheck: true, // Disables host header check (useful for local dev, but not recommended for production)
+  // Disables host header check (useful for local dev, but not recommended for production)
     allowedHosts: 'all', // Use this if you're accessing from a custom domain (if needed)
   },
 });
